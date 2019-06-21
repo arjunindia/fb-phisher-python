7c50c5843431c7576ebdff71724410dcad5e72bd
Arjun India <37960163+arjunindia@users.noreply.github.com>
2019-06-21 10:10:48 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+echo "Installer loading"
+chmod +x relinker.sh
+sleep 2s
+rm /usr/bin/facebooker&>/dev/null
+echo "#!/bin/bash"> /usr/bin/facebooker
+path=$PWD
+echo "pather='$path'">>/usr/bin/facebooker
+echo "cd '$path'">>/usr/bin/facebooker
+echo "$path/relinker.sh">>/usr/bin/facebooker
+echo "python3 $path/app.py">>/usr/bin/facebooker
+chmod +x /usr/bin/facebooker
+echo "FINISHED INSTALLATION!"
+echo "RUN faceboker FROM ANYWHERE TO EXECUTE THE TOOL!"
